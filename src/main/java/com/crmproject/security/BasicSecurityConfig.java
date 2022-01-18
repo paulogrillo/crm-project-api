ecee805a57f27cfc8f9d9091cb5d4bc2318f806f
paulogrillo <paulodigitalart@gmail.com>
2022-01-18 18:24:41 -0300

    Update class BasicSecurityConfig.java

--- src/main/java/com/crmproject/security/BasicSecurityConfig.java
+++ src/main/java/com/crmproject/security/BasicSecurityConfig.java
@@ -40,7 +40,6 @@ public class BasicSecurityConfig  extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/demo").permitAll()
 		.antMatchers("/**").permitAll()
 		.antMatchers("/user").permitAll()
 		.antMatchers("/user/logar").permitAll()
