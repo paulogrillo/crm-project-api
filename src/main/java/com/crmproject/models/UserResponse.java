d03507c5358d3306eff53807aa2a05ebcf9d7698
paulogrillo <paulodigitalart@gmail.com>
2022-01-18 17:50:28 -0300

    Update package models add Class UserResponse.java

---
+++ src/main/java/com/crmproject/models/UserResponse.java
@@ -0,0 +1,15 @@
+package com.crmproject.models;
+
+public class UserResponse {
+	
+	private String message;
+	
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
