86aa5fbb0e0916b44b0bb34d56fff5da9b365534
paulogrillo <paulodigitalart@gmail.com>
2022-01-19 20:17:39 -0300

    Update class ClientController.java

--- src/main/java/com/crmproject/controllers/ClientController.java
+++ src/main/java/com/crmproject/controllers/ClientController.java
@@ -6,14 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crmproject.models.Client;
+import com.crmproject.models.User;
 import com.crmproject.repository.ClientRepository;
 
 @RestController
@@ -24,19 +27,43 @@ public class ClientController {
 	@Autowired
 	private ClientRepository repository;
 	
+	//getALL
 	@GetMapping
 	public ResponseEntity<List<Client>>getAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
 	
-	@GetMapping("/client/{nameClient}")
-	public ResponseEntity<List<Client>> getByClientName(@PathVariable long nameClient){
+	//getId
+	@GetMapping("/{id}")
+	public ResponseEntity<List<Client>> getByClientId(@PathVariable long id){
+		 return ResponseEntity.ok(repository.findAllById(
+				  id));
+	}
+	
+	//getName
+	@GetMapping("/{nameClient}")
+	public ResponseEntity<List<Client>> getByClientName(@PathVariable String nameClient){
 		 return ResponseEntity.ok(repository.findAllByNameClientContainingIgnoreCase(
 				  nameClient));
 	}
 	
+	//Post
 	@PostMapping
 	public ResponseEntity<Client> post(@RequestBody Client client){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(client));
 	}
+	
+	//Put
+	@PutMapping
+	public ResponseEntity<Client> put(@RequestBody Client client) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(client));
+	}
+	
+	//Delete
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
+	
 }
