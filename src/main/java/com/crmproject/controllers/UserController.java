55cde579a7b60c2fe59742aa914704a9938e43b8
paulogrillo <paulodigitalart@gmail.com>
2022-01-19 20:18:09 -0300

    Update class UserController.java

--- src/main/java/com/crmproject/controllers/UserController.java
+++ src/main/java/com/crmproject/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.crmproject.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
